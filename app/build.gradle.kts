df43a6e282dfa41022157847f083ab0e169ddaeb
Developer <dev@coffeediary.local>
2026-05-23 21:55:11 +0800

    v0.1.2.4 build release APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.example.coffeediary"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.1.2.3"
+        versionCode = 7
+        versionName = "0.1.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
